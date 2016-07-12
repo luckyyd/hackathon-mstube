e4d5b060f3aed18188d40f43b86366f3c5b76ffd
luckyyd <luckyyd@tju.edu.cn>
2016-07-12 17:36:30 +0800

    获取Candidates， 将用户Preference数据存入Redis

--- WebAPI/mstube/mstube/Controllers/ApiController.cs
+++ WebAPI/mstube/mstube/Controllers/ApiController.cs
@@ -21,19 +21,31 @@ namespace mstube.Controllers
         [HttpGet]
         public JsonResult Candidates()
         {
+            //Return a random recommend list for test
             StreamReader sr = new StreamReader(Server.MapPath(@"~/App_Data/items.json"));
-
             var json = JsonConvert.DeserializeObject<List<Item.Item>>(sr.ReadToEnd());
-
             return Json(json, JsonRequestBehavior.AllowGet);
         }
 
-        [HttpPost]
-        public async Task<JsonResult> Preference(Preference.Preference pre)
+        [HttpGet]
+        public async Task<JsonResult> Candidates(long id)
         {
+            //Get new id for user_id
+
             //Send POST request to Azure ML
-            string result = await Utils.AzureML.SendPOSTRequest(pre.user_id);
-            return Json(result);
+            string result = await Utils.AzureML.SendPOSTRequest(id);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Preference(Preference.Preference pre)
+        {
+            //Write preference data to Redis
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("mstube-dotnet.redis.cache.windows.net,abortConnect=false,ssl=true,password=6/Cq0R6Wh+L6PJeYI80KEMVyYVGUjqZFEnNS6iJHl1A=");
+            IDatabase cache = connection.GetDatabase();
+            string preStr = pre.user_id.ToString() + pre.item_id.ToString();
+            cache.StringSet(preStr, pre.score);
+            return Json(pre);
         }
     }
 }
