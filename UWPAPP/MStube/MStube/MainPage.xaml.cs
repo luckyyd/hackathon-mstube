d66497354b726d870346b64f7c1c6f7f4b6d4beb
irmowan <irmowan@gmail.com>
2016-07-18 13:50:32 +0800

    Load json file and display

--- UWPAPP/MStube/MStube/MainPage.xaml.cs
+++ UWPAPP/MStube/MStube/MainPage.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,12 +23,51 @@ namespace MStube
             public string VideoTitle { get; set; }
             public string Description { get; set; }
         }
+        public class VideoItem
+        {
+            public int id;
+            public string url;
+            public string description;
+            public string image_src;
+            public string title;
+        }
         private List<VideoBrief> listOfVideoBrief = new List<VideoBrief>();
+        static List<VideoItem> LoadVideoItem() {
+            string path = @"json/shows.json";
+            string text = "";
+            List<VideoItem> items = new List<VideoItem>();
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Debug.WriteLine("File exists.");
+                    text = System.IO.File.ReadAllText(path);
+                    Debug.WriteLine(text);
+                    items = JsonConvert.DeserializeObject<List<VideoItem>>(text);
+                }
+                else
+                {
+                    Debug.WriteLine("File not exists.");
+                }
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Exception thrown.");
+            }
+            return items;
+        }
         public MainPage()
         {
             this.InitializeComponent();
-            listOfVideoBrief.Add(new VideoBrief { ImageSourceUri = "https://www.google.com.sg/images/branding/product/ico/googleg_lodp.ico", VideoTitle = "Google", Description = "Hello, Google." });
-            listOfVideoBrief.Add(new VideoBrief { ImageSourceUri = "https://www.microsoft.com/favicon.ico?v2", VideoTitle = "Microsoft", Description = "Hi, Microsoft." });
+            Debug.WriteLine("Test file");
+            // Read the JSON file.
+            var t = Task.Run(() => LoadVideoItem());
+            t.Wait();
+            List<VideoItem> items = t.Result;
+            foreach (VideoItem item in items)
+            {
+                listOfVideoBrief.Add(new VideoBrief { ImageSourceUri = item.image_src, VideoTitle = item.title, Description = item.description });
+            }
             VideoBriefList.ItemsSource = listOfVideoBrief;
         }
         private void hyperlinkButton_Click(object sender, RoutedEventArgs e)
